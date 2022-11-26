147ac6e5d68670f3171fbb37b55a4c91694fd815
IsaacIndex <isaacwongnh@gmail.com>
2022-11-27 04:22:04 +0800

    set up private docker on oracle

--- oci-infra/infra.tf
+++ oci-infra/infra.tf
@@ -205,3 +205,10 @@ resource "oci_containerengine_node_pool" "k8s_node_pool" {
   }
   ssh_public_key = var.ssh_public_key
 }
+
+resource "oci_artifacts_container_repository" "docker_repository" {
+  compartment_id = var.compartment_id
+  display_name   = "free-kubernetes-nginx"
+  is_immutable = false
+  is_public    = false
+}
