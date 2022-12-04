67174ebe7dd3095d4fb6cada1fe5450f0ef735ae
IsaacIndex <isaacwongnh@gmail.com>
2022-12-04 22:54:12 +0800

    update use another version

--- oci-infra/infra.tf
+++ oci-infra/infra.tf
@@ -77,7 +77,7 @@ resource "oci_core_security_list" "public_subnet_sl" {
     destination_type = "CIDR_BLOCK"
     protocol         = "all"
   }
-
+  
   egress_security_rules {
     stateless        = false
     destination      = "10.0.1.0/24"
@@ -135,6 +135,7 @@ resource "oci_core_subnet" "vcn_private_subnet" {
   compartment_id = var.compartment_id
   vcn_id         = module.vcn.vcn_id
   cidr_block     = "10.0.1.0/24"
+
   route_table_id             = module.vcn.nat_route_id
   security_list_ids          = [oci_core_security_list.private_subnet_sl.id]
   display_name               = "free-k8s-private-subnet"
@@ -145,6 +146,7 @@ resource "oci_core_subnet" "vcn_public_subnet" {
   compartment_id = var.compartment_id
   vcn_id         = module.vcn.vcn_id
   cidr_block     = "10.0.0.0/24"
+
   route_table_id    = module.vcn.ig_route_id
   security_list_ids = [oci_core_security_list.public_subnet_sl.id]
   display_name      = "free-k8s-public-subnet"
@@ -155,10 +157,12 @@ resource "oci_containerengine_cluster" "k8s_cluster" {
   kubernetes_version = "v1.21.5"
   name               = "free-k8s-cluster"
   vcn_id             = module.vcn.vcn_id
+
   endpoint_config {
     is_public_ip_enabled = true
     subnet_id            = oci_core_subnet.vcn_public_subnet.id
   }
+
   options {
     add_ons {
       is_kubernetes_dashboard_enabled = false
@@ -176,39 +180,62 @@ data "oci_identity_availability_domains" "ads" {
   compartment_id = var.compartment_id
 }
 
+locals {
+  # Gather a list of availability domains for use in configuring placement_configs
+  azs = data.oci_identity_availability_domains.ads.availability_domains[*].name
+}
+
+data "oci_core_images" "latest_image" {
+  compartment_id = var.compartment_id
+  operating_system = "Oracle Linux"
+  operating_system_version = "7.9"
+  filter {
+    name   = "display_name"
+    values = ["^.*aarch64-.*$"]
+    regex = true
+  }
+}
+
 resource "oci_containerengine_node_pool" "k8s_node_pool" {
   cluster_id         = oci_containerengine_cluster.k8s_cluster.id
   compartment_id     = var.compartment_id
   kubernetes_version = "v1.21.5"
   name               = "free-k8s-node-pool"
   node_config_details {
-    placement_configs {
-      availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-      subnet_id           = oci_core_subnet.vcn_private_subnet.id
+    dynamic placement_configs {
+      for_each = local.azs
+      content {
+        availability_domain = placement_configs.value
+        subnet_id           = oci_core_subnet.vcn_private_subnet.id
+      }
     }
-    // how many nodes I want to provision in this node-pool
     size = 2
+
   }
   node_shape = "VM.Standard.A1.Flex"
+
   node_shape_config {
-    # RAM Memory
     memory_in_gbs = 6
     ocpus         = 1
   }
+
   node_source_details {
-    image_id    = "ocid1.image.oc1.ap-sydney-1.aaaaaaaaq2imz2yce564e5x32gfvycu3evgywf422nfllkb3thm2ufczstzq"
+    image_id    = data.oci_core_images.latest_image.images.0.id
     source_type = "image"
   }
+
   initial_node_labels {
     key   = "name"
     value = "free-k8s-cluster"
   }
+
   ssh_public_key = var.ssh_public_key
 }
 
 resource "oci_artifacts_container_repository" "docker_repository" {
   compartment_id = var.compartment_id
   display_name   = "free-kubernetes-nginx"
+
   is_immutable = false
   is_public    = false
 }
